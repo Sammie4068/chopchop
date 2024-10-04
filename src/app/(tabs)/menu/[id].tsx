1d066288378a4dcab9195fa6ae9351d950172fd4
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-10-04 08:26:11 +0100

    product details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,96 @@
+import Button from "@/components/Button";
+import { defaultPizzaImage } from "@/components/ProductList";
+import products from "@assets/data/products";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+
+const sizes = ["S", "M", "L", "XL"];
 
 export default function ProductDetailsScreen() {
   const { id } = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState("M");
+
+  const product = products.find((product) => product.id.toString() === id);
+
+  if (!product) return <Text>Product not found</Text>;
+
+  function addToCart() {
+    console.warn("Adding to cart, size: ", selectedSize);
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: `Details ${id}` }} />
-      <Text>{id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            key={size}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize === size ? "gainsboro" : "white",
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                {
+                  color: selectedSize === size ? "black" : "gray",
+                },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text style={styles.price}>NGN {product.price}</Text>
+      <Button onPress={addToCart} text="Add to Cart" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
